4e159874562c19b47d7c91a4f5b427b9c7675659
gaujimann <gudjong19@ru.is>
2020-12-11 20:21:58 +0000

    styling for movie details

---
+++ src/views/MovieDetails/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  imageContainer: {
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  poster: {
+    height: 350,
+    width: 175,
+  },
+  container: {
+    margin: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  titleContainer: {
+    borderStyle: 'solid',
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgb(155, 155, 155)',
+    padding: 8,
+    marginRight: 8,
+    marginLeft: 8,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+});
